351ba3160ad35a1baae9e7bd05d2e6a82c981909
AzazelN28 <asakon28@gmail.com>
2015-12-12 22:59:56 +0100

    Removed test directory from git and modified test.sh to call first tar.sh and install.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-# This script
+# Create slush-rojo2.tgz
+./tar.sh
+
+# Install slush-rojo2.tgz
+./install.sh
+
+# Create "test" directory if doesn't exists
 if [ ! -d "test" ]; then
   mkdir "test"
 fi
 
+# check if slush exists, if not
+# then install slush globally and also
+# install slush-rojo2
 which slush
 if [ $? != 0 ]; then
-  npm install -g -f slush slush-rojo2
+  npm install -g -f slush
 fi
 
+# enter in "test" directory
 cd "test"
+
+# remove everything from "test" directory
 rm -rf *
+
+# This answers the questions from slush
+# automaticly
 slush rojo2 <<< "test
 
 
@@ -19,4 +34,6 @@ slush rojo2 <<< "test
 
 
 "
+
+# run build script
 npm run build
